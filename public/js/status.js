df75bd37b3fe5a1362c145cb1a30653e5a6d5905
Jesse Zhu <jesse@itjesse.cn>
2015-03-11 02:55:39 +0800

    添加时间选择

--- public/js/status.js
+++ public/js/status.js
@@ -1,3 +1,51 @@
+var showChart = function(renderTo, res){
+    renderTo.highcharts({
+        chart: {
+            type: 'spline',
+            zoomType: 'x'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                minute: '%H:%M'
+            }
+        },
+        yAxis: {
+            title: {
+                text: ''
+            },
+
+            minorGridLineWidth: 0,
+            gridLineWidth: 1,
+            alternateGridColor: null
+        },
+        tooltip: {
+            formatter: function() {
+                return '' +
+                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br><b>' + this.y + '</b>';
+            }
+        },
+        plotOptions: {
+            spline: {
+                marker: {
+                    enabled: false
+                }
+            }
+        },
+        series: [{
+            data: res
+        }],
+        navigation: {
+            menuItemStyle: {
+                fontSize: '10px'
+            }
+        }
+    });
+};
+
 $(document).ready(function() {
     var chartWidth = $("#chartDiv").width();
     var chartHeight = $("#chartDiv").height();
@@ -14,51 +62,7 @@ $(document).ready(function() {
             data: 'id=' + id + '&startTime=',
             renderTo: $(this),
             success: function(res) {
-                this.renderTo.highcharts({
-                    chart: {
-                        type: 'spline',
-                        zoomType: 'x'
-                    },
-                    title: {
-                        text: ''
-                    },
-                    xAxis: {
-                        type: 'datetime',
-                        dateTimeLabelFormats: {
-                            minute: '%H:%M'
-                        }
-                    },
-                    yAxis: {
-                        title: {
-                            text: ''
-                        },
-
-                        minorGridLineWidth: 0,
-                        gridLineWidth: 1,
-                        alternateGridColor: null
-                    },
-                    tooltip: {
-                        formatter: function() {
-                            return '' +
-                                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br><b>' + this.y + '</b>';
-                        }
-                    },
-                    plotOptions: {
-                        spline: {
-                            marker: {
-                                enabled: false
-                            }
-                        }
-                    },
-                    series: [{
-                        data: res
-                    }],
-                    navigation: {
-                        menuItemStyle: {
-                            fontSize: '10px'
-                        }
-                    }
-                });
+                showChart(this.renderTo, res);
             }
         });
     });
@@ -78,4 +82,22 @@ $(document).ready(function() {
         });
     });
 
+    $('.timeSelect').on('click', function(){
+        $(this).parent().parent().find('li').each(function(){
+            $(this).removeClass('active');
+        });
+        $(this).parent().addClass('active');
+        var chartId = $(this).data("chartid");
+        var startTime = $(this).data("starttime");
+        $.ajax({
+            url: './api/GetChartVal',
+            type: 'get',
+            dataType: 'json',
+            data: 'id=' + chartId + '&startTime=' + startTime,
+            renderTo: $('div[data-id='+chartId+']'),
+            success: function(res) {
+                showChart(this.renderTo, res);
+            }
+        });
+    });
 });
--- routes/getChartVal.js
+++ routes/getChartVal.js
@@ -6,7 +6,7 @@ var mysql = require('../modules/mysql');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var id = req.query.id;
-    var startTime = req.query.startTime ? req.query.startTime : (new Date()).valueOf() - 1 * 60 * 60 * 1000;
+    var startTime = req.query.startTime ? req.query.startTime : (new Date()).valueOf() - 10 * 60 * 1000;
     var sql = "SELECT xbee_data.`value`, UNIX_TIMESTAMP(xbee_data.`time`) AS time FROM xbee_data,xbee_list WHERE xbee_data.mac = xbee_list.mac AND xbee_data.type = xbee_list.type AND xbee_list.id = '" + id + "' AND xbee_data.time > " + startTime + " ORDER BY xbee_data.time ASC";
     mysql.query(sql, function(err, rows) {
         if (err) return console.log(err);
