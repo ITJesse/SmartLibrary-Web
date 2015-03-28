aa7e10aefdfe353cabee5d8a2694db030b21322d
Jesse Zhu <jesse@itjesse.cn>
2015-03-28 15:47:58 +0800

    修复时区错误

--- routes/getChartVal.js
+++ routes/getChartVal.js
@@ -7,7 +7,7 @@ var mysql = require('../modules/mysql');
 router.get('/', function(req, res, next) {
     var id = req.query.id;
     var startTime = req.query.startTime ? Math.ceil(((new Date()).valueOf() - req.query.startTime) / 1000) : Math.ceil(((new Date()).valueOf() - 10 * 60 * 1000) / 1000);
-    var sql = "SELECT UNIX_TIMESTAMP(xbee_data.time) * 1000 AS time, xbee_data.`value` FROM xbee_list INNER JOIN xbee_data ON xbee_list.mac = xbee_data.mac AND xbee_data.type = xbee_list.type WHERE xbee_list.id = " + id + " AND UNIX_TIMESTAMP(xbee_data.time) > " + startTime + " ORDER BY xbee_data.time ASC";
+    var sql = "SELECT (UNIX_TIMESTAMP(xbee_data.time) + 8 * 60 * 60) * 1000 AS time, xbee_data.`value` FROM xbee_list INNER JOIN xbee_data ON xbee_list.mac = xbee_data.mac AND xbee_data.type = xbee_list.type WHERE xbee_list.id = " + id + " AND UNIX_TIMESTAMP(xbee_data.time) > " + startTime + " ORDER BY xbee_data.time ASC";
     mysql.query(sql, function(err, rows) {
         if (err) return console.log(err);
         var data = [];
