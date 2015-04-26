e6c2d8c770590e6ec141c4c03f51316fe4f73b47
Jesse Zhu <jesse@itjesse.cn>
2015-04-27 00:36:13 +0800

    环境监测页面报错检测

--- public/js/environment.js
+++ public/js/environment.js
@@ -1,87 +1,91 @@
+var options = {
+    grid: {
+        show: true,
+        aboveData: true,
+        color: "#3f3f3f",
+        labelMargin: 5,
+        axisMargin: 0,
+        borderWidth: 0,
+        borderColor: null,
+        minBorderMargin: 5,
+        clickable: true,
+        hoverable: true,
+        autoHighlight: true,
+        mouseActiveRadius: 20
+    },
+    series: {
+        grow: {
+            active: true,
+            stepMode: "linear",
+            steps: 50,
+            stepDelay: true
+        },
+        lines: {
+            show: true,
+            fill: true,
+            lineWidth: 4,
+            steps: false
+        },
+        points: {
+            show: true,
+            radius: 5,
+            symbol: "circle",
+            fill: true,
+            borderColor: "#fff"
+        }
+    },
+    legend: {
+        position: "ne",
+        margin: [0, -25],
+        noColumns: 0,
+        labelBoxBorderColor: null,
+        labelFormatter: function(label, series) {
+            // just add some space to labes
+            return label + '&nbsp;&nbsp;';
+        }
+    },
+    xaxis: {
+        mode: "time",
+        timeformat: "%m/%d %H:%M"
+    },
+    yaxis: {
+        min: 0
+    },
+    colors: chartColours,
+    shadowSize: 1,
+    tooltip: true, //activate tooltip
+    tooltipOpts: {
+        content: "%s : %y.1",
+        shifts: {
+            x: -30,
+            y: -50
+        }
+    }
+};
+
 var getChartValues = function(chart, sensorId){
     $.ajax({
         type: "GET",
         url: "/API/Web/GetChartVal?id="+sensorId+"&startTime=9999999999",
 		dataType: "json",
         success: function(msg) {
-            var data = msg.data;
-            var label = msg.label;
-            var options = {
-                grid: {
-                    show: true,
-                    aboveData: true,
-                    color: "#3f3f3f",
-                    labelMargin: 5,
-                    axisMargin: 0,
-                    borderWidth: 0,
-                    borderColor: null,
-                    minBorderMargin: 5,
-                    clickable: true,
-                    hoverable: true,
-                    autoHighlight: true,
-                    mouseActiveRadius: 20
-                },
-                series: {
-                    grow: {
-                        active: true,
-                        stepMode: "linear",
-                        steps: 50,
-                        stepDelay: true
-                    },
-                    lines: {
-                        show: true,
-                        fill: true,
-                        lineWidth: 4,
-                        steps: false
-                    },
-                    points: {
-                        show: true,
-                        radius: 5,
-                        symbol: "circle",
-                        fill: true,
-                        borderColor: "#fff"
+            if(!msg.error){
+                var data = msg.data;
+                var label = msg.label;
+                $.plot(chart, [{
+                        label: label,
+                        data: data,
+                        lines: {
+                            fillColor: "#f2f7f9"
+                        },
+                        points: {
+                            fillColor: "#88bbc8"
+                        }
                     }
-                },
-                legend: {
-                    position: "ne",
-                    margin: [0, -25],
-                    noColumns: 0,
-                    labelBoxBorderColor: null,
-                    labelFormatter: function(label, series) {
-                        // just add some space to labes
-                        return label + '&nbsp;&nbsp;';
-                    }
-                },
-                xaxis: {
-                    mode: "time",
-                    timeformat: "%m/%d %H:%M"
-                },
-                yaxis: {
-                    min: 0
-                },
-                colors: chartColours,
-                shadowSize: 1,
-                tooltip: true, //activate tooltip
-                tooltipOpts: {
-                    content: "%s : %y.1",
-                    shifts: {
-                        x: -30,
-                        y: -50
-                    }
-                }
-            };
-            $.plot(chart, [{
-                    label: label,
-                    data: data,
-                    lines: {
-                        fillColor: "#f2f7f9"
-                    },
-                    points: {
-                        fillColor: "#88bbc8"
-                    }
-                }
-
-            ], options);
+                ], options);
+            }else{
+                console.log(msg);
+            }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             console.log("Error" + XMLHttpRequest.responseText);
