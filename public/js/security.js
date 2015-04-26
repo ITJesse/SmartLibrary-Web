90938b6c0973d9151a384d563da848eff6aa6b87
Jesse Zhu <jesse@itjesse.cn>
2015-04-27 04:38:05 +0800

    修改采集间隔为1s

--- public/js/security.js
+++ public/js/security.js
@@ -2,5 +2,5 @@ $(document).on('ready', function() {
     setInterval(function(){
         var timestamp = (new Date()).valueOf();
         $('#liveCamera').attr('src', '/API/Web/GetCameraImage?' + timestamp);
-    }, 2000);
+    }, 1000);
 });
