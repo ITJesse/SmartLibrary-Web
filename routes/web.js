c9ba2aae074e6cba88dac2f8b3f0ba4b799212ab
Jesse Zhu <jesse@itjesse.cn>
2015-04-27 05:55:29 +0800

    删除多余的括号

--- routes/web.js
+++ routes/web.js
@@ -8,7 +8,7 @@ var config = require('../modules/config');
 /* GET home page. */
 router.get('/GetChartVal', function(req, res) {
     var id = req.query.id;
-    var startTime = parseInt(req.query.startTime) ? Math.ceil((new Date()).valueOf() / 1000) - req.query.startTime : Math.ceil(()(new Date()).valueOf() - 10 * 60 * 1000) / 1000);
+    var startTime = parseInt(req.query.startTime) ? Math.ceil((new Date()).valueOf() / 1000) - req.query.startTime : Math.ceil(((new Date()).valueOf() - 10 * 60 * 1000) / 1000);
     var sql = "SELECT (UNIX_TIMESTAMP(xbee_data.time) + 8 * 60 * 60) * 1000 AS time, xbee_data.`value` FROM xbee_list INNER JOIN xbee_data ON xbee_list.mac = xbee_data.mac AND xbee_data.type = xbee_list.type WHERE xbee_list.id = " + id + " AND UNIX_TIMESTAMP(xbee_data.time) > " + startTime + " ORDER BY xbee_data.time ASC";
     mysql.query(sql, function(err, rows) {
         if(err){
