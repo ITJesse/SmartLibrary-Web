6e67c551af7fb598d2566da413887e083ca6d7be
Jesse Zhu <jesse@itjesse.cn>
2015-04-27 06:30:01 +0800

    添加实时摄像头的开关

--- public/js/security.js
+++ public/js/security.js
@@ -1,6 +1,41 @@
+var liveCameraInterval;
+
+var resize = function() {
+    var width = $('#liveCameraContainer').width();
+    $('#liveCamera').width(width);
+    $('#liveCamera').height(width * 0.75);
+}
+
+var startLiveCamera = function(){
+    var timestamp = (new Date()).valueOf();
+    $('#liveCamera').attr('src', '/API/Web/GetCameraImage?' + timestamp);
+}
+
 $(document).on('ready', function() {
-    setInterval(function(){
-        var timestamp = (new Date()).valueOf();
-        $('#liveCamera').attr('src', '/API/Web/GetCameraImage?' + timestamp);
-    }, 1000);
+
+    $('.iToggle-button').toggleButtons({
+	    width: 70,
+	    label: {
+	        enabled: "<span class='icon16 icomoon-icon-checkmark-2 white'></span>",
+	        disabled: "<span class='icon16 icomoon-icon-close white marginL5'></span>"
+	    }
+	});
+
+    liveCameraInterval = setInterval(startLiveCamera, 1000);
+
+    $('#liveCameraSwitch').on('change', function(){
+        if($(this).attr('checked') != 'checked'){
+            clearInterval(liveCameraInterval);
+        }else{
+            clearInterval(liveCameraInterval);
+            liveCameraInterval = setInterval(startLiveCamera, 1000);
+        }
+    });
+
+    resize();
+
+});
+
+$(window).on('resize', function() {
+    resize();
 });
